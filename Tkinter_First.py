25e4aa4e97e1247d92a1dfd37bed9696bd17f522
Kanber Buldu <kanberbuldu@gmail.com>
2024-01-12 08:54:18 +0300

    Add Tkinter Exam

---
+++ Tkinter_First.py
@@ -0,0 +1,17 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Firs Tkinter Exam")
+window.hei
+
+my_label = tkinter.Label(text="My First Label")
+my_label.pack()
+
+my_button = tkinter.Button(text = "Enter")
+my_button.pack()
+
+
+
+
+
+tkinter.mainloop()
